21a967dc258f9e36269f7741ba3463c0d7b634b0
Dmitry Buyan <dmitry.buyan@MacBook-Air.local>
2021-06-28 22:33:51 +0400

    добавил анимацию ошибки для формы

--- js/feedback.js
+++ js/feedback.js
@@ -2,6 +2,7 @@ const writeUsButton = document.querySelector(".contacts__button");
 const feedback = document.querySelector(".feedback");
 const userNameField = feedback.querySelector("#contact-username");
 const userEmail = feedback.querySelector("#contact-email");
+const userFeedback = feedback.querySelector("#contact-text");
 const feedbackCloseButton = feedback.querySelector(".feedback__button-close");
 const feedbackForm = feedback.querySelector(".feedback__form");
 
@@ -23,14 +24,21 @@ const feedbackHide = function(evt) {
 const validateForm = function(evt) {
   if (!userNameField.value) {
     evt.preventDefault();
+    feedback.classList.add("feedback--error");
   } else {
     localStorage.setItem("username", userNameField.value);
   }
 
   if (!userEmail.value) {
     evt.preventDefault();
+    feedback.classList.add("feedback--error");
   } else {
     localStorage.setItem("email", userEmail.value);
+  }
+
+  if (!userFeedback.value) {
+    evt.preventDefault();
+    feedback.classList.add("feedback--error");
   }
 }
 
